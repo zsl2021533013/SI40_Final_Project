9b1d93ff45af20b1caf58885f997edd13334a1a5
zsl2021533013 <105686154+zsl2021533013@users.noreply.github.com>
2023-01-13 23:45:47 +0800

    有依赖的抽取

--- Part1/bandit.py
+++ Part1/bandit.py
@@ -6,7 +6,6 @@ def pull_arm(i) -> int:
 
     assert 1 <= i <= 3
 
-
     tmp = random.random()
     match i:
         case 1:
@@ -15,3 +14,15 @@ def pull_arm(i) -> int:
             return tmp < 0.5
         case 3:
             return tmp < 0.4
+
+def pull_arm_dependent(i) -> int:
+    """传入摇杆位置 i，返回结果 0/1，在摇动任何摇杆后，
+    该摇杆下一次获奖几率变为 1，其余两个变为 0，初始值为 0 0 0"""
+    if not hasattr(pull_arm_dependent, 'former_choice'): #静态变量
+        pull_arm_dependent.former_choice = 0
+    if i == pull_arm_dependent.former_choice:
+        pull_arm_dependent.former_choice = i
+        return 1
+    else:
+        pull_arm_dependent.former_choice = i
+        return 0
--- Part1/main.py
+++ Part1/main.py
@@ -1,16 +1,23 @@
 import epsilon_greedy
 import thompson_sampling_algorithm
 import ucb_algorithm
+import bandit
 
-n = 5000
-loop_time = 200
+# n = 5000
+# loop_time = 200
+#
+# ans = [0 for _ in range(4)]
+# for i in range(loop_time):
+#     ans[1] += epsilon_greedy.run(n, 0.1) / n
+#     ans[2] += ucb_algorithm.run(n, 1) / n
+#     ans[3] += thompson_sampling_algorithm.run(n, [601, 401, 401, 601, 2, 3]) / n
+#
+# print(ans[1] / loop_time)
+# print(ans[2] / loop_time)
+# print(ans[3] / loop_time)
 
-ans = [0 for _ in range(4)]
-for i in range(loop_time):
-    ans[1] += epsilon_greedy.run(n, 0.1) / n
-    ans[2] += ucb_algorithm.run(n, 1) / n
-    ans[3] += thompson_sampling_algorithm.run(n, [601, 401, 401, 601, 2, 3]) / n
-
-print(ans[1] / loop_time)
-print(ans[2] / loop_time)
-print(ans[3] / loop_time)
+print(bandit.pull_arm_dependent(1))
+print(bandit.pull_arm_dependent(1))
+print(bandit.pull_arm_dependent(2))
+print(bandit.pull_arm_dependent(2))
+print(bandit.pull_arm_dependent(2))
